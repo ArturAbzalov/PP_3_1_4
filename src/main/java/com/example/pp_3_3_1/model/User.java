6050c9910a0dd5745093154f5a71849d8871a6c1
Artur Abzalov <mr.art-abzalov@yandex.ru>
2022-04-30 22:59:03 +0300

    add

---
+++ src/main/java/com/example/pp_3_3_1/model/User.java
@@ -0,0 +1,53 @@
+package com.example.pp_3_3_1.model;
+
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+    public User() {
+
+    }
+
+    public User(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
